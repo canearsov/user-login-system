61aad7b0de84f09fd75d948565e81bb500453cd2
canearsov <canearsov@gmail.com>
2025-11-18 20:23:24 +0100

    Popravi environment variable za MongoDB Atlas

--- config/db.js
+++ config/db.js
@@ -3,8 +3,16 @@ const mongoose = require("mongoose");
 async function connectDB() {
   if (mongoose.connection.readyState === 1) return;
 
-  await mongoose.connect(process.env.MONGODB_URI);
-  console.log("Connected to MongoDB Atlas");
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB Atlas");
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1);
+  }
 }
 
 module.exports = connectDB;
