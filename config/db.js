e7f72f593b2144e219f640c5014bbfff79ae0e7e
canearsov <126823657+canearsov@users.noreply.github.com>
2026-02-14 18:12:54 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,10 +4,7 @@ async function connectDB() {
   if (mongoose.connection.readyState === 1) return;
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB Atlas");
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
